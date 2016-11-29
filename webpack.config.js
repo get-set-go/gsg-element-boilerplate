20a65d77f0786eeb81fe1b7d899ce923f59af1c4
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-30 00:53:59 +0530

    Configs Uglifyjs, banner define plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var webpack = require('webpack');
+var product = require('./package.json');
+
+var banner = product.name + " v" + product.version;
+banner += "\nDeveloped & maintained by " + product.author + " and contributors."
+banner += "\nMIT Licensed"
+
 var config = {
   entry: './src/index.js',
 
@@ -19,12 +26,27 @@ var config = {
     }, {
       test: /\.css$/,
       loader: 'style-loader!css-loader!postcss-loader'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
     }]
   },
 
   postcss: function() {
     return [require('autoprefixer'), require('precss')];
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin(),
+
+    new webpack.BannerPlugin(banner)
+  ]
 };
 
 module.exports = config;
